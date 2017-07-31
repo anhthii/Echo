f7048e56c6a3514a38c38fb2c3e2d54e0d85babb
Neo Esil <nguyendanganhthi247@gmail.com>
2017-07-31 08:19:46 +0700

    add require_auth middleware to playlist endpoint

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const media = require('./media');
 const user = require('./user');
 const playlist = require('./playlist');
+const requireAuth = require('middlewares/require_authentication');
 
 const router = express.Router();
 
 router.use('/media', media);
 router.use('/user', user);
-router.use('/playlist', playlist);
+router.use('/playlist', requireAuth, playlist);
 
 module.exports = router;
