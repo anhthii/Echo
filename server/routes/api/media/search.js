484809561f51f4dc294d01e10e7a69f75b2241bc
Thi <nguyendanganhthi247@gmail.com>
2021-06-27 19:25:19 +0700

    add extractAlias util

--- server/routes/api/media/search.js
+++ server/routes/api/media/search.js
@@ -4,12 +4,11 @@ const ZingMP3 = require('../../../lib/ZingMP3')
 module.exports = function (req, res, next) {
   const { term } = req.query;
   const url = ZingMP3.composeURL(ZingMP3.V2.resources.search,{q: term});
-  
+
   request(url)
     .then(response => {
       response = JSON.parse(response);
       res.send(response);
     })
     .catch(err => next(err));
-
 };
