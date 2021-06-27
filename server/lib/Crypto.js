4c8aaffa3043af75d3bd61ad13d5036e4aa90b2d
Thi <nguyendanganhthi247@gmail.com>
2021-06-27 11:39:56 +0700

    add Zingmp3 library

--- server/lib/Crypto.js
+++ server/lib/Crypto.js
@@ -2,13 +2,12 @@ const crypto = require('crypto');
 
 
 module.exports = {
-
-    getHash256: function(a){
-        return crypto.createHash('sha256').update(a).digest('hex');
-    },
-    getHmac512: function(str, key){
-        let hmac = crypto.createHmac("sha512", key);
-        return hmac.update(Buffer.from(str, 'utf8')).digest("hex");
-    }
+  createHash256: function(str) {
+    return crypto.createHash('sha256').update(str).digest('hex');
+  },
+  createHmac512: function(str, key) {
+    let hmac = crypto.createHmac("sha512", key);
+    return hmac.update(Buffer.from(str, 'utf8')).digest("hex");
+  }
 }
 
