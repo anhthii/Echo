d9651ce0544b818dd33a026a2814a58a6cc1bfa3
Lucnguyenkhmt <ancucchocon@gmail.com>
2021-07-04 08:18:44 +0700

    update version mongoose

--- server/lib/Database.js
+++ server/lib/Database.js
@@ -5,7 +5,7 @@ exports.init = function () {
   return new Promise((resolve, reject) => {
     mongoose.Promise = global.Promise;
     const conn = mongoose.connection;
-    mongoose.connect(process.env.MONGODB_URI);
+    mongoose.connect(process.env.MONGODB_URI , {useNewUrlParser: true, useUnifiedTopology: true});
     conn.on('error', (err) => reject(err));
     conn.once('open', () => resolve());
   });
